c98d4649d20c825755f7af3acdb7676c618c5b29
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-03-28 01:48:50 +0300

    Completed Function Continued Lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+};
+mondayWork();
+mondayWork("work from home");
+
+const wrapAdjective = function (star = "*") {
+  return function (greeting = "special") {
+    return `You are ${star}${greeting}${star}!`;
+  };
+};
+console.log(wrapAdjective("*")("a FullStack Web Developer"));
